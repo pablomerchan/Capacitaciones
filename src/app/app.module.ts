261a68818c36a9eb3161b76a2a0a9528d24a9d10
Pablo Merchan <pablomerchan@gmail.com>
2023-12-06 15:05:00 -0500

    feat: Creacion de los modulos padre e hijo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './components/padre/padre.component';
+import { HijoComponent } from './components/hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule,
